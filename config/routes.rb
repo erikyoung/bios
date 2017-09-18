5c9a0c9203058efd80645e7dc1559ce5b0f63b45
erikyoung <erik.young@rmit.edu.vn>
2017-09-19 00:20:19 +0700

    added initial bootstrap for modal

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 	root 'bios#index'
 	resources :bios do
 	resources :photos
+
+	
 end
 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
