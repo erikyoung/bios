f4e9de016c52a968f4bd59ec8c360baa1a111808
erikyoung <erik.young@rmit.edu.vn>
2017-10-18 23:41:37 +0700

    added hero stories link

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 	resources :comments
 end
 	resources :photos
+	resources :stories
 end
 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
---
+++ db/migrate/20171018163549_create_stories.rb
@@ -0,0 +1,10 @@
+class CreateStories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :stories do |t|
+    	t.string :title
+    	t.text :body
+    	t.integer :user_id
+      t.timestamps
+    end
+  end
+end
