4ec4743ebc98a9fc8288d78e2189ab2d0c267e54
erikyoung <erik.young@rmit.edu.vn>
2017-10-18 23:07:06 +0700

    added contact button in collapsed info

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,12 @@ class CommentsController < ApplicationController
 	def create
 		@bio = Bio.find(params[:bio_id])
 		@bio.comments.create(comment_params.merge(user: current_user))
+		if @current_user.save
+			flash[:success] = "New Comment Created!"
 		redirect_to bio_path(@bio)
+		else
+			render 'new'
+		end
 	end
 
 	private
